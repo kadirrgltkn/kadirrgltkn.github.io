4ae531f6ec7471c12b6936294e03e66af42bac9f
Kadir Gültekin <143501442+kadirrgltkn@users.noreply.github.com>
2024-01-04 14:48:58 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -17,30 +17,34 @@ const elements = {
 
 const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
 
+lanyard.onopen = () => {
+    console.log("WebSocket connection established.");
+
+    // Initial check for user's current status
+    lanyard.send(
+        JSON.stringify({
+            op: OPCODES.INIT,
+            d: {
+                subscribe_to_id: "737196145431937036",
+            },
+        })
+    );
+};
+
 // On Message
 lanyard.onmessage = ({ data }) => {
     const parsedData = JSON.parse(data);
 
-    if (parsedData.op == OPCODES.HELLO) {
-        // Identify
-        lanyard.send(
-            JSON.stringify({
-                op: OPCODES.INIT,
-                d: {
-                    subscribe_to_id: "737196145431937036",
-                },
-            })
-        );
-
+    if (parsedData.op === OPCODES.HELLO) {
         // Interval
-        setInterval(function () {
+        setInterval(() => {
             lanyard.send(
                 JSON.stringify({
                     op: OPCODES.HEARTBEAT,
                 })
             );
         }, parsedData.d.heartbeat_interval);
-    } else if (parsedData.op == OPCODES.INFO) {
+    } else if (parsedData.op === OPCODES.INFO) {
         const statusColors = {
             online: "#2afa62",
             offline: "#747F8D",
@@ -48,51 +52,54 @@ lanyard.onmessage = ({ data }) => {
             dnd: "#ff3640",
         };
 
-        if (parsedData.t == "INIT_STATE") {
+        if (parsedData.t === "INIT_STATE") {
             const user = parsedData.d;
+            elements.status.style.background = statusColors[user.discord_status];
             elements.avatar.src = `https://cdn.discordapp.com/avatars/737196145431937036/${user.discord_user.avatar}?size=128`;
-            var a = 0
-            console.log(user)
+
+            let a = 0;
+            console.log(user);
             elements.card.style.opacity = "1";
             elements.username.innerText = user.discord_user.username;
             elements.discriminator.innerText = `#${user.discord_user.discriminator}`;
+
             try {
-            if (!user.activities[1]){
-            a += 2
+                if (!user.activities[1]) {
+                    a += 2;
+                }
+                if (!user.activities[0]) {
+                    a += 1;
+                }
+                if (a === 3) {
+                    elements.durum.innerText = "Discord Status: Anything";
+                } else if (a === 2) {
+                    elements.durum.innerText = "Discord Status: " + user.activities[0].state;
+                } else if (a === 0) {
+                    elements.durum.innerText =
+                        "Discord Status: " + user.activities[0].state + "\n What am I doing: " + user.activities[1].name;
+                }
+
+                if (user.listening_to_spotify === true) {
+                    document.getElementById("spotify-artist").innerText = "\n" + user.spotify.artist;
+                    document.getElementById("spotify-song").innerText = "\n" + user.spotify.song;
+                    document.getElementById("spotify-pic").src = user.spotify.album_art_url;
+                    document.getElementById("spotify-pic").style.visibility = "visible";
+                } else if (user.activities[1]) {
+                    document.getElementById("avatar").style.cursor = "pointer";
+                }
+            } catch (error) {
+                document.getElementById("playingname").innerText = "No Status Available";
+                console.log(error);
             }
-            if (!user.activities[0]){
-            a += 1
-            } if (a == 3){
-            elements.durum.innerText ="Discord Status: Anything"
-            } else if ( a == 2) {
-            elements.durum.innerText ="Discord Status: "+ user.activities[0].state
-            } else if (a == 0){
-            elements.durum.innerText ="Discord Status: "+ user.activities[0].state+"\n What am I doing: "+user.activities[1].name
-            
-            }
-            if(user.listening_to_spotify== true){
-                document.getElementById("spotify-artist").innerText = "\n"+ user.spotify.artist
-                document.getElementById("spotify-song").innerText = "\n"+ user.spotify.song
-                document.getElementById("spotify-pic").src = user.spotify.album_art_url
-                document.getElementById("spotify-pic").style.visibility = "visible";
-            }
-            
-           else if(user.activities[1]){
-            document.getElementById("avatar").style.cursor = "pointer"
-            }
-                
-            } catch(error) {
-            document.getElementById("playingname").innerText ="No Status Available"
-            console.log(error)
-            }
-            elements.status.style.background =
-                statusColors[user.discord_status];
-            
-            
-        } else if (parsedData.t == "PRESENCE_UPDATE") {
+
+            elements.status.style.background = statusColors[user.discord_status];
+        } else if (parsedData.t === "PRESENCE_UPDATE") {
             const user = parsedData.d;
-            elements.status.style.background =
-                statusColors[user.discord_status];
+            elements.status.style.background = statusColors[user.discord_status];
         }
     }
+};
+
+lanyard.onclose = (event) => {
+    console.log(`WebSocket connection closed: ${event.reason}`);
 };
